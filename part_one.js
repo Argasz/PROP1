0d79037ce118c642c26617cb718881f76a79f077
arga1535 <argasz@gmail.com>
2018-11-26 15:14:27 +0100

    Test med part_one

--- part_one.js
+++ part_one.js
@@ -1,8 +1,15 @@
 myObject = {
     create : function(prototypeList){
-        return {protoList : prototypeList,
-                call : this.call,
-                create : this.create}
+        var protoList = prototypeList;
+        if(!protoList){
+            protoList = [];
+        }
+        return {protoList : protoList,
+            call : this.call,
+            create : this.create,
+            addPrototype : this.addPrototype,
+            hasProto : this.hasProto,
+            _superCall : this._superCall}
     },
     call : function(funcName, parameters){
         if(this.hasOwnProperty(funcName)){
@@ -12,27 +19,56 @@ myObject = {
                 return this[funcName].apply(null, parameters);
             }
         }else{
-            for(var i = 0; i < this.protoList.length; i++){
-                var returnVal = this.protoList[i].call(funcName, parameters);
-                if(returnVal){
+            for (i = 0; i < this.protoList.length; i++) {
+                var returnVal = this.protoList[i]._superCall(funcName, parameters);
+                if (returnVal) {
                     return returnVal;
                 }
             }
-            throw "No function with the name " + funcName + "was found.";
+            throw this.constructor.name + " has no property " + funcName + ".";
+        }
+    },
+    _superCall : function(funcName, parameters){
+        if(this.hasOwnProperty(funcName)){
+            if(parameters === undefined || parameters.length === 0){
+                return this[funcName]();
+            }else{
+                return this[funcName].apply(null, parameters);
+            }
+        }else{
+            for (i = 0; i < this.protoList.length; i++) {
+                var returnVal = this.protoList[i]._superCall(funcName, parameters);
+                if (returnVal) {
+                    return returnVal;
+                }
+            }
+            return null;
+        }
+    },
+    addPrototype : function(proto){
+        if(proto.hasProto(this)){
+            throw "Circular inheritance."
+        }
+        if(typeof proto === "object"){
+            this.protoList.push(proto);
+        }else{
+            throw "Prototype must be of type object."
+        }
+    },
+    hasProto : function(obj){
+        if(this.protoList.includes(obj)){
+            return true;
+        }else{
+            for(i = 0; i < this.protoList.length; i++){
+                var hasProto = this.protoList[i].hasProto(obj);
+                if(hasProto){
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
 };
 
-
-var o1 = myObject.create([]);
-o1.func = function(arg){return "func0: " + arg; };
-
-var o2 = myObject.create([o1]);
-o2.func2 = function(arg){return "func1: " + arg; };
-
-var o3 = myObject.create([o2]);
-
-
-console.log(o3.call("func", ["Hi"]));
 
